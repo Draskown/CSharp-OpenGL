9fe23052b399145d946d400cc21565feb573a965
Andryuha <astik452@gmail.com>
2021-06-06 20:44:51 +0300

    Working map of points cloud and skybox

---
+++ EP_Form.Designer.cs
@@ -0,0 +1,139 @@
+﻿
+namespace EP
+{
+    partial class EP_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(EP_Form));
+            this.frameGL = new SharpGL.OpenGLControl();
+            this.numMin = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numMax = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.frameGL)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numMin)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numMax)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // frameGL
+            // 
+            this.frameGL.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.frameGL.DrawFPS = false;
+            this.frameGL.Location = new System.Drawing.Point(16, 16);
+            this.frameGL.Name = "frameGL";
+            this.frameGL.OpenGLVersion = SharpGL.Version.OpenGLVersion.OpenGL2_1;
+            this.frameGL.RenderContextType = SharpGL.RenderContextType.DIBSection;
+            this.frameGL.RenderTrigger = SharpGL.RenderTrigger.TimerBased;
+            this.frameGL.Size = new System.Drawing.Size(680, 665);
+            this.frameGL.TabIndex = 0;
+            // 
+            // numMin
+            // 
+            this.numMin.ForeColor = System.Drawing.Color.White;
+            this.numMin.Location = new System.Drawing.Point(712, 105);
+            this.numMin.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numMin.Name = "numMin";
+            this.numMin.Size = new System.Drawing.Size(140, 23);
+            this.numMin.TabIndex = 1;
+            this.numMin.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(736, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Min thresh";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(736, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Max thresh";
+            // 
+            // numMax
+            // 
+            this.numMax.ForeColor = System.Drawing.Color.White;
+            this.numMax.Location = new System.Drawing.Point(712, 41);
+            this.numMax.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numMax.Name = "numMax";
+            this.numMax.Size = new System.Drawing.Size(140, 23);
+            this.numMax.TabIndex = 1;
+            this.numMax.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.numMax.Value = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            // 
+            // EP_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(866, 605);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numMax);
+            this.Controls.Add(this.numMin);
+            this.Controls.Add(this.frameGL);
+            this.Font = new System.Drawing.Font("Gilroy", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ForeColor = System.Drawing.Color.White;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "EP_Form";
+            this.Text = "Application";
+            this.Load += new System.EventHandler(this.EP_Form_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.frameGL)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numMin)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numMax)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private SharpGL.OpenGLControl frameGL;
+        private System.Windows.Forms.NumericUpDown numMin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numMax;
+    }
+}
+
---
+++ EP_Form.cs
@@ -0,0 +1,267 @@
+﻿using SharpGL;
+using SharpGL.SceneGraph.Assets;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EP
+{
+    public partial class EP_Form : Form
+    {
+        #region Initialization
+
+        private readonly OpenGL gl;
+
+        private readonly Texture[] textures;
+
+        private readonly Bitmap coloredImage, depthMap;
+
+        private PointF delta, angle, initPoint;
+
+        private Color color;
+
+        private readonly float angleDelta, scale, cubeSize;
+        private float distance;
+
+        private readonly int imageIndex, pointSize;
+        private int threshMin, threshMax, depth;
+
+        public EP_Form()
+        {
+            InitializeComponent();
+
+            gl = this.frameGL.OpenGL;
+
+            textures = new Texture[7];
+            for (int i = 0; i < textures.Length; i++)
+                textures[i] = new Texture();
+
+            imageIndex = 7;
+            pointSize = 2;
+
+            depthMap = new Bitmap(Image.FromFile($"Images/bmps/{imageIndex}_Map.bmp"));
+            coloredImage = new Bitmap(
+                Image.FromFile($"Images/bmps/{imageIndex}_Color.bmp"),
+                new Size(depthMap.Width, depthMap.Height));
+
+            delta = new PointF(0.0f, 0.0f);
+            angle = new PointF(0.0f, 0.0f);
+
+            angleDelta = 0.02f;
+            distance = 6.0f;
+            scale = 100.0f;
+            cubeSize = 50.0f;
+
+            InitTextures();
+        }
+
+
+        private void EP_Form_Load(object sender, EventArgs e)
+        {
+            this.frameGL.FrameRate = 60;
+            this.frameGL.MouseDown += (ss, ee) => initPoint = ee.Location;
+            this.frameGL.MouseWheel += ChangeDistance;
+            this.frameGL.MouseMove += ChangePosition;
+            this.frameGL.OpenGLDraw += Draw;
+        }
+
+        #endregion
+
+
+        private void InitTextures()
+        {
+            textures[0].Create(gl, new Bitmap(Image.FromFile($"Images/Textures/negx.jpg"), new Size(512, 512)));
+            textures[1].Create(gl, new Bitmap(Image.FromFile($"Images/Textures/posx.jpg"), new Size(512, 512)));
+            textures[2].Create(gl, new Bitmap(Image.FromFile($"Images/Textures/negy.jpg"), new Size(512, 512)));
+            textures[3].Create(gl, new Bitmap(Image.FromFile($"Images/Textures/negz.jpg"), new Size(512, 512)));
+            textures[4].Create(gl, new Bitmap(Image.FromFile($"Images/Textures/negx.jpg"), new Size(512, 512)));
+            textures[5].Create(gl, new Bitmap(Image.FromFile($"Images/Textures/posy.jpg"), new Size(512, 512)));
+            textures[6].Create(gl, new Bitmap(Image.FromFile($"Images/Textures/posz.jpg"), new Size(512, 512)));
+        }
+
+
+        #region Drawing
+
+        private void Draw(object sender, RenderEventArgs args)
+        {
+            gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);
+            gl.LoadIdentity();
+
+            threshMin = (int)numMin.Value;
+            threshMax = (int)numMax.Value;
+
+            gl.Translate(delta.X, delta.Y, -distance);
+            gl.Rotate(angle.Y, angle.X, 0.0f);
+
+            gl.Enable(OpenGL.GL_DEPTH_TEST);
+            gl.Enable(OpenGL.GL_ALPHA_TEST);
+
+            gl.PushMatrix();
+            gl.Color(1.0f, 1.0f, 1.0f, 1.0f);
+            gl.Enable(OpenGL.GL_TEXTURE_2D);
+            BindTextures();
+            gl.Disable(OpenGL.GL_TEXTURE_2D);
+            gl.PopMatrix();
+
+            gl.Translate(0.0f, 0.0f, -100.0f);
+            gl.PointSize(pointSize);
+            gl.Begin(OpenGL.GL_POINTS);
+            {
+                gl.Color(1.0f, 1.0f, 1.0f);
+                gl.Vertex(0.0f, 0.0f, 0.0f);
+
+                for (int x = 0; x < coloredImage.Width; x+=pointSize)
+                    for (int y = 0; y < coloredImage.Height; y+=pointSize)
+                    {
+                        color = coloredImage.GetPixel(x, y);
+                        depth = (int)depthMap.GetPixel(x, y).R;
+
+                        if (depth <= threshMin || depth >= threshMax)
+                            continue;
+
+                        var p = UVD2XYZ(x,y,depth);
+
+                        gl.Color(color.R, color.G, color.B);
+                        gl.Vertex(p[0] / scale, p[1] / scale, scale - p[2] / scale);
+                    }
+            }
+        }
+
+        #endregion
+
+
+
+        private float[] UVD2XYZ(float u, float v, float d)
+        {
+            var xyz = new float[3];
+
+            var cx = 1.5f;
+            var cy = 2.4273913761751615e+02f;
+            var fx = 1.0f / 5.9421434211923247e+02f;
+            var fy = 1.0f / 5.9104053696870778e+02f;
+
+            xyz[0] = (u - cx) * d * fx;
+            xyz[1] = (v - cy) * d * fy;
+            xyz[2] = d;
+
+            xyz[1] *= (float)Math.Cos(Math.PI);
+            xyz[2] *= (float)Math.Cos(Math.PI);
+
+            return xyz;
+        }
+
+
+        private void BindTextures()
+        {
+            textures[0].Bind(gl);
+            gl.Begin(SharpGL.Enumerations.BeginMode.Quads);
+            {
+                gl.TexCoord(0.0f, 0.0f); gl.Vertex(-cubeSize, cubeSize, cubeSize);
+                gl.TexCoord(1.0f, 0.0f); gl.Vertex(-cubeSize, cubeSize, -cubeSize);
+                gl.TexCoord(1.0f, 1.0f); gl.Vertex(-cubeSize, -cubeSize, -cubeSize);
+                gl.TexCoord(0.0f, 1.0f); gl.Vertex(-cubeSize, -cubeSize, cubeSize);
+            }
+            gl.End();
+
+            textures[1].Bind(gl);
+            gl.Begin(SharpGL.Enumerations.BeginMode.Quads);
+            {
+                gl.TexCoord(0.0f, 0.0f); gl.Vertex(-cubeSize, cubeSize, cubeSize);
+                gl.TexCoord(1.0f, 0.0f); gl.Vertex(-cubeSize, cubeSize, -cubeSize);
+                gl.TexCoord(1.0f, 1.0f); gl.Vertex(-cubeSize, -cubeSize, -cubeSize);
+                gl.TexCoord(0.0f, 1.0f); gl.Vertex(-cubeSize, -cubeSize, cubeSize);
+            }
+            gl.End();
+
+            textures[2].Bind(gl);
+            gl.Begin(SharpGL.Enumerations.BeginMode.Quads);
+            {
+                gl.TexCoord(0.0f, 0.0f); gl.Vertex(cubeSize, -cubeSize, cubeSize);
+                gl.TexCoord(1.0f, 0.0f); gl.Vertex(-cubeSize, -cubeSize, cubeSize);
+                gl.TexCoord(1.0f, 1.0f); gl.Vertex(-cubeSize, -cubeSize, -cubeSize);
+                gl.TexCoord(0.0f, 1.0f); gl.Vertex(cubeSize, -cubeSize, -cubeSize);
+            }
+            gl.End();
+
+            textures[3].Bind(gl);
+            gl.Begin(SharpGL.Enumerations.BeginMode.Quads);
+            {
+                gl.TexCoord(0.0f, 0.0f); gl.Vertex(-cubeSize, cubeSize, -cubeSize);
+                gl.TexCoord(1.0f, 0.0f); gl.Vertex(cubeSize, cubeSize, -cubeSize);
+                gl.TexCoord(1.0f, 1.0f); gl.Vertex(cubeSize, -cubeSize, -cubeSize);
+                gl.TexCoord(0.0f, 1.0f); gl.Vertex(-cubeSize, -cubeSize, -cubeSize);
+            }
+            gl.End();
+
+            textures[4].Bind(gl);
+            gl.Begin(SharpGL.Enumerations.BeginMode.Quads);
+            {
+                gl.TexCoord(0.0f, 0.0f); gl.Vertex(cubeSize, cubeSize, -cubeSize);
+                gl.TexCoord(1.0f, 0.0f); gl.Vertex(cubeSize, cubeSize, cubeSize);
+                gl.TexCoord(1.0f, 1.0f); gl.Vertex(cubeSize, -cubeSize, cubeSize);
+                gl.TexCoord(0.0f, 1.0f); gl.Vertex(cubeSize, -cubeSize, -cubeSize);
+            }
+            gl.End();
+
+            textures[5].Bind(gl);
+            gl.Begin(SharpGL.Enumerations.BeginMode.Quads);
+            {
+                gl.TexCoord(0.0f, 0.0f); gl.Vertex(cubeSize, cubeSize, -cubeSize);
+                gl.TexCoord(1.0f, 0.0f); gl.Vertex(-cubeSize, cubeSize, -cubeSize);
+                gl.TexCoord(1.0f, 1.0f); gl.Vertex(-cubeSize, cubeSize, cubeSize);
+                gl.TexCoord(0.0f, 1.0f); gl.Vertex(cubeSize, cubeSize, cubeSize);
+            }
+            gl.End();
+
+            textures[6].Bind(gl);
+            gl.Begin(SharpGL.Enumerations.BeginMode.Quads);
+            {
+                gl.TexCoord(0.0f, 0.0f); gl.Vertex(cubeSize, cubeSize, cubeSize);
+                gl.TexCoord(1.0f, 0.0f); gl.Vertex(-cubeSize, cubeSize, cubeSize);
+                gl.TexCoord(1.0f, 1.0f); gl.Vertex(-cubeSize, -cubeSize, cubeSize);
+                gl.TexCoord(0.0f, 1.0f); gl.Vertex(cubeSize, -cubeSize, cubeSize);
+            }
+            gl.End();
+        }
+
+
+
+        #region Mouse moving
+
+        private void ChangePosition(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                PointF finalPoint = e.Location;
+
+                if (ModifierKeys == Keys.Shift)
+                {
+                    angle.X += (finalPoint.X - initPoint.X) * angleDelta;
+                    angle.Y += (finalPoint.Y - initPoint.Y) * angleDelta;
+                }
+                else
+                {
+                    delta.X += (finalPoint.X - initPoint.X) / 100;
+                    delta.Y -= (finalPoint.Y - initPoint.Y) / 100;
+                }
+
+                initPoint = finalPoint;
+            }
+        }
+
+
+        private void ChangeDistance(object sender, MouseEventArgs e)
+        {
+            distance -= e.Delta / 120;
+        }
+
+        #endregion
+    }
+}
